24ac515fecb7ef0792b34394afc2151cf36e4cf9
wallenium <wallenium@51303710-8900-4070-9444-ab6cb0b6b11d>
2013-12-02 18:59:42 +0000

    - add beta version of elder scrolls online
    git-svn-id: svn://svn.code.sf.net/p/eqdkp-plus/code/games/trunk@13735 51303710-8900-4070-9444-ab6cb0b6b11d

---
+++ eso/language/german.php
@@ -0,0 +1,47 @@
+<?php
+ /*
+ * Project:		EQdkp-Plus
+ * License:		Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
+ * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * -----------------------------------------------------------------------
+ * Began:		2012
+ * Date:		$Date$
+ * -----------------------------------------------------------------------
+ * @author		$Author$
+ * @copyright	2006-2012 EQdkp-Plus Developer Team
+ * @link		http://eqdkp-plus.com
+ * @package		eqdkp-plus
+ * @version		$Rev$
+ * 
+ * $Id$
+ */
+
+if ( !defined('EQDKP_INC') ){
+	header('HTTP/1.0 404 Not Found');exit;
+}
+$german_array = array(
+	'classes' => array(
+		0	=> 'Unbekannt',
+		1	=> 'Drachenritter',
+		2	=> 'Templer',
+		3	=> 'Magier',
+		4	=> 'Wächter',
+	),
+	'races' => array(
+		'Hochelfen', 'Waldelfen', 'Khajiit',	// Aldmeri
+		'Bretonen', 'Rothwardonen', 'Orks',		// Dolchsturz
+		'Nord', 'Dunkelelfen', 'Argonier',		// Ebenherz
+	),
+	'factions' => array(
+		'Aldmeri-Dominion',
+		'Dolchsturz-Bündnis',
+		'Ebenherz-Pakt',
+	),
+	'lang' => array(
+		'eso'					=> 'The Elder Scrolls Online',
+		'pk_tab_fs_esosettings'	=> 'Elder Scrolls Einstellungen',
+		'eso_faction'			=> 'Fraktionen',
+	),
+);
+
+?>
