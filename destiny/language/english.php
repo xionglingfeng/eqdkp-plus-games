33090e3adb0a053e2e4084d07c99b09b4ab34193
wallenium <wallenium@51303710-8900-4070-9444-ab6cb0b6b11d>
2014-10-13 18:06:33 +0000

    - ported the game for 2.0
    git-svn-id: svn://svn.code.sf.net/p/eqdkp-plus/code/games/trunk@14651 51303710-8900-4070-9444-ab6cb0b6b11d

---
+++ destiny/language/english.php
@@ -0,0 +1,45 @@
+<?php
+ /*
+ * Project:		EQdkp-Plus
+ * License:		Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
+ * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * -----------------------------------------------------------------------
+ * Began:		2009
+ * Date:		$Date$
+ * -----------------------------------------------------------------------
+ * @author		$Author$
+ * @copyright	2006-2011 EQdkp-Plus Developer Team
+ * @link		http://eqdkp-plus.com
+ * @package		eqdkp-plus
+ * @version		$Rev$
+ * 
+ * $Id$
+ */
+
+if ( !defined('EQDKP_INC') ){
+	header('HTTP/1.0 404 Not Found');exit;
+}
+$english_array = array(
+	'classes' => array(
+		0 => 'Unknown',
+		1 => 'Titan',
+		2 => 'Warlock',
+		3 => 'Hunter',
+	),
+	'races' => array(
+		0 => 'Unknown',
+		1 => 'Human',
+		2 => 'Awoken',
+		3 => 'Exo',
+	),
+	'lang' => array(
+		'destiny'					=> 'Destiny',
+		'core_sett_fs_gamesettings'	=> 'Destiny Settings',
+		
+		// Roles
+		'role1'						=> 'Guardian',
+		'role2'						=> 'Range',
+		'role3'						=> 'Melee',
+	),
+);
+?>
