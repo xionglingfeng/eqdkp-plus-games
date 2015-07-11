1db08d03317ce440393cae725f9f9f429e41dadc
xionglingfeng <xionglingfeng@Gmail.com>
2015-07-11 03:16:17 -0230

    [+] Add Chinese Simplified translation for EQ

---
+++ eq/game-eq-2.2.2/language/chinesesimplified.php
@@ -0,0 +1,78 @@
+<?php
+ /*
+ * Project:		EQdkp-Plus
+ * License:		Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
+ * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * -----------------------------------------------------------------------
+ * Began:		2009
+ * Date:		$Date$
+ * -----------------------------------------------------------------------
+ * @author		$Author$
+ * @copyright	2006-2011 EQdkp-Plus Developer Team
+ * @link		http://eqdkp-plus.com
+ * @package		eqdkp-plus
+ * @version		$Rev$
+ * 
+ * $Id$
+ */
+
+if ( !defined('EQDKP_INC') ){
+    header('HTTP/1.0 404 Not Found');
+    exit;
+}
+$chinesesimplified_array = array(
+	'classes' => array(
+		0	=> '未知',
+		1	=> '吟游诗人',
+		2	=> 'Beastlord',
+		3	=> 'Berserker',
+		4	=> 'Enchanter',
+		5	=> '魔法师',
+		6	=> '武僧',
+		7	=> '亡灵巫师',
+		8	=> '圣骑士',
+		9	=> 'Ranger',
+		10	=> '盗贼',
+		11	=> 'Shadow Knight',
+		12	=> '洒满祭司',
+		13	=> '战士',
+		14	=> '巫师',
+		15	=> '牧师',
+		16	=> '德鲁伊',
+	),
+	'races' => array(
+		0	=> '未知',
+		1	=> '侏儒',
+		2	=> '人类',
+		3	=> '野蛮人',
+		4	=> '矮人',
+		5	=> '高等精灵',
+		6	=> '黑暗精灵',
+		7	=> '木精灵',
+		8	=> '半精灵',
+		9	=> '猫人',
+		10	=> '巨魔',
+		11	=> '食人魔',
+		12	=> 'Frog',
+		13	=> '蜥蜴人',
+		14	=> '博学者',
+		15	=> '半身人',
+		16	=> '龙人'
+	),
+	'lang' => array(
+		'eq'						=> '无尽的任务',
+		'plate'						=> '板甲',
+		'silk'						=> '布甲',
+		'leather'					=> '皮甲',
+		'chain'						=> '链甲',
+		'uc_race'					=> '种族',
+		'uc_class'					=> '职业',
+
+		// Admin Settings
+		'core_sett_fs_gamesettings'	=> '无尽的任务设置',
+		'uc_faction'				=> '阵营',
+		'uc_faction_help'			=> '选择默认阵营',
+	),
+);
+
+?>
